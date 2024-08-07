512331b59b369e8128fa8dfc24297f890b7484ac
sunnyychungg <s.chung@2022.ljmu.ac.uk>
2024-08-07 02:14:10 +0100

    Functionality to change grid button, allowing user to select grid size, \n button removes grid and regenerates a new grid with the a normal smaller resolution.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
 const grid = document.querySelector(".grid-container");
 
-function createGrid() {
-    for (let i = 0; i < 50; i++) { 
+function createGrid(gridSize = 16) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    };
+
+    for (let i = 0; i < gridSize; i++) { 
         // Creates Y axis, 0 to 16 vertically.
         const yAxis = document.createElement("div");
         yAxis.classList.add("y" + i);
@@ -12,7 +15,7 @@ function createGrid() {
         // Selects current Y axis grid
         const currentY = document.querySelector(".y" + i);
 
-        for (let j = 0; j < 50; j++) {
+        for (let j = 0; j < gridSize; j++) {
             // Creates 0 to 16 boxes horizontally, across X axis
             const xAxis = document.createElement("div");
             xAxis.classList.add("x" + j);
@@ -26,5 +29,22 @@ function createGrid() {
         }
     }
 }
+
+function sizeGrid() {
+    size = prompt("What size would you like? \n Example: 20 give an 20x20 grid \n MAX: 100x100 ");
+    size = parseInt(size);
+    if (size > 100) {
+        alert("Try again");
+    }
+    else {
+        createGrid(size);
+    }
+}
+
+const gridSize = document.querySelector('#gridSize')
+
+gridSize.addEventListener('click', () => {
+    sizeGrid()
+});
 
 createGrid();
